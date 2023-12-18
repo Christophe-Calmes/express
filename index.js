7c334cd67109f5a77bccae72b5827c0db6bfd704
Calmes <christophe.calmes@laplateforme.io>
2023-12-18 16:03:29 +0100

    Nodemon

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Express");
+});
+
 app
   .listen(port, () => {
     console.info(`Server is listening on port ${port}`);
@@ -8,3 +13,8 @@ app
   .on("error", (err) => {
     console.error("Error:", err.message);
   });
+  const welcomeName = (req, res) => {
+    res.send(`Welcome ${req.params.name}`);
+  };
+  
+  app.get("/users/:name", welcomeName);
