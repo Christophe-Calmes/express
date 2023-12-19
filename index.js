f0adecbda9206cefd603d658f7519e8bbb59ea35
Calmes <christophe.calmes@laplateforme.io>
2023-12-19 09:33:05 +0100

    Challenge Express01

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
+
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Express");
-});
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    color: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    color: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+const getMovies = (req, res) => {
+  res.status(200).json(movies);
+};
+const getOneMovie = (req, res) => {
+    const id = parseInt(req.params.id);
+    const response = movies.find((element) => element.id === id);
+    console.log(`response =  ${response}`);
+    console.log(response);
+    if(response != undefined) {
+      res.status(200).json(response);
+    } else {
+      res.status(404).send('Not found');
+    }
+}
 
+app.get("/", (req, res) => {
+  res.send("Welcome to my favourite movie list");
+});
+app.get("/api/movies", getMovies);
+app.get("/api/movies/:id", getOneMovie)
 app
   .listen(port, () => {
     console.info(`Server is listening on port ${port}`);
@@ -13,8 +57,3 @@ app
   .on("error", (err) => {
     console.error("Error:", err.message);
   });
-  const welcomeName = (req, res) => {
-    res.send(`Welcome ${req.params.name}`);
-  };
-  
-  app.get("/users/:name", welcomeName);
